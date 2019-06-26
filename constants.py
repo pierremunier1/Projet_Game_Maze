8e15ba476f393a917d168fc74f3ab66904f8511b
unknown <pierremunier@hotmail.fr>
2019-06-26 23:07:15 +0200

    **modify constant**

---
+++ constants.py
@@ -0,0 +1,11 @@
+"""Constants of MacGyver labyrinth game"""
+
+#Game window's size
+sprite_width_num = 15
+sprite_height_num = 15
+sprite_size = 40
+
+#Labyrinth structure symbols
+wall_symbol = 'X'
+floor_symbol = ' '
+object_symbol = 'o'
