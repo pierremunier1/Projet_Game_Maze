c34288696e49c87d68774a1404f11904913d9286
unknown <pierremunier@hotmail.fr>
2019-06-28 14:48:18 +0200

    initial commit

---
+++ classes.py
@@ -0,0 +1,28 @@
+class Labyrinth:
+     """Classe définissant la structure du  Labyrinthe par:
+    - sa largeur
+    - sa hauteur
+    - sa structure"""
+
+     
+def __init__(self):
+        self.width = 15
+        self.height = 15
+        self.labyrinthe =["XXXXXXXXXXXXXXXX",
+                          "XXXXXXXXXXXXXXXX",
+                          "XXXXXXXXXXXXXXXX",
+                          "XXXXXXXXXXXXXXXX",
+                          "XXXXXXXXXXXXXXXX",
+                          "XXXXXXXXXXXXXXXX",
+                          "XXXXXXXXXXXXXXXX",
+                          "XXXXXXXXXXXXXXXX",
+                          "XXXXXXXXXXXXXXXX",
+                          "XXXXXXXXXXXXXXXX",
+                          "XXXXXXXXXXXXXXXX",
+                          "XXXXXXXXXXXXXXXX",
+                          "XXXXXXXXXXXXXXXX",
+                          "XXXXXXXXXXXXXXXX",
+                          "XXXXXXXXXXXXXXXX",]
+                  
+                                                               
+
