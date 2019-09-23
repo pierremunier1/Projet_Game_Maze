5b7486bb47fb1c709920566c38afb665118ee8ca
Pierre Munier <pierremunier@hotmail.fr>
2019-09-23 12:30:59 +0200

    flake8 check

--- labyrinth.py
+++ labyrinth.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from position import Position
 from items import Items
 import constants
@@ -8,9 +10,8 @@ class Maze:
     """Class for building and showing the maze"""
 
     def __init__(self):
+        # initializing list and position of the maze"""
 
-        """initializing list and position of the maze"""
-       
         self.start = None
         self.end = None
         self.hero = None
@@ -24,12 +25,9 @@ class Maze:
         self.needle = []
         self.items = []
         self.inventory = []
-    
-        
-    def build(self,filename):
-        
-        """Method building the maze when reading the text file 'laby.txt' and append all
-           character into list or position"""
+
+    def build(self, filename):
+        # Method building the maze when reading the text file 'laby.txt'
 
         with open(filename) as laby:
             for n_line, line in enumerate(laby):
@@ -42,58 +40,43 @@ class Maze:
                         self.end = position
                     elif c == "P":
                         self.free.append(position)
-                        self.hero = position                       
+                        self.hero = position
                     elif c == ".":
                         self.free.append(position)
                     elif c == "#":
                         self.wall.append(position)
                     elif c == "G":
                         self.guardian = position
-                        self.free.append(position) 
-         
+                        self.free.append(position)
+
             self.width = n_column + 1
             self.height = n_line + 1
 
         print()
 
     def show(self):
-        
-        """Method display the maze in the terminal console with 'print' function"""
-    
+        # Method display the maze in the console with 'print' function
+
         for y in range(self.height):
             for x in range(self.width):
                 position = Position(x, y)
                 if position == self.start:
-                    print("S",end='')
+                    print("S", end='')
                 elif position in self.ether:
-                    print("€",end='')
+                    print("€", end='')
                 elif position in self.needle:
-                    print("N",end='')
+                    print("N", end='')
                 elif position in self.tube:
-                    print("T",end='')
+                    print("T", end='')
                 elif position == self.hero:
-                    print("P",end='')
+                    print("P", end='')
                 elif position == self.guardian:
-                    print("G",end='')
+                    print("G", end='')
                 elif position == self.end:
-                    print("E",end='')
+                    print("E", end='')
                 elif position in self.free:
-                    print(".",end='')
+                    print(".", end='')
                 elif position in self.wall:
-                    print("#",end='')
+                    print("#", end='')
 
             print()
-            
-            
-        
-    
-
-            
-
-
-           
-
-
-
-                        
-
